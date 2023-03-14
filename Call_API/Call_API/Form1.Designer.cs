29dc0fbfe3554323d54ebe4f75ef33f9fdd250f7
tuyen2001 <tuyenkhong2001@gmail.com>
2023-03-14 19:15:37 +0700

    init project

---
+++ Call_API/Call_API/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿
+namespace Call_API
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_id = new System.Windows.Forms.TextBox();
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.txt_price = new System.Windows.Forms.TextBox();
+            this.txt_quantity = new System.Windows.Forms.TextBox();
+            this.btn_Add = new System.Windows.Forms.Button();
+            this.btn_update = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.btn_close = new System.Windows.Forms.Button();
+            this.show = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.show)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(258, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(258, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(258, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Price";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(258, 230);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Quantity";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(337, 19);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "QUẢN LÝ NƯỚC HOA";
+            // 
+            // txt_id
+            // 
+            this.txt_id.Location = new System.Drawing.Point(340, 68);
+            this.txt_id.Name = "txt_id";
+            this.txt_id.Size = new System.Drawing.Size(100, 20);
+            this.txt_id.TabIndex = 5;
+            // 
+            // txt_name
+            // 
+            this.txt_name.Location = new System.Drawing.Point(340, 122);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(100, 20);
+            this.txt_name.TabIndex = 6;
+            // 
+            // txt_price
+            // 
+            this.txt_price.Location = new System.Drawing.Point(340, 176);
+            this.txt_price.Name = "txt_price";
+            this.txt_price.Size = new System.Drawing.Size(100, 20);
+            this.txt_price.TabIndex = 7;
+            // 
+            // txt_quantity
+            // 
+            this.txt_quantity.Location = new System.Drawing.Point(340, 230);
+            this.txt_quantity.Name = "txt_quantity";
+            this.txt_quantity.Size = new System.Drawing.Size(100, 20);
+            this.txt_quantity.TabIndex = 8;
+            // 
+            // btn_Add
+            // 
+            this.btn_Add.Location = new System.Drawing.Point(581, 68);
+            this.btn_Add.Name = "btn_Add";
+            this.btn_Add.Size = new System.Drawing.Size(75, 23);
+            this.btn_Add.TabIndex = 9;
+            this.btn_Add.Text = "Add";
+            this.btn_Add.UseVisualStyleBackColor = true;
+            this.btn_Add.Click += new System.EventHandler(this.btn_Add_Click);
+            // 
+            // btn_update
+            // 
+            this.btn_update.Location = new System.Drawing.Point(581, 122);
+            this.btn_update.Name = "btn_update";
+            this.btn_update.Size = new System.Drawing.Size(75, 23);
+            this.btn_update.TabIndex = 10;
+            this.btn_update.Text = "Update";
+            this.btn_update.UseVisualStyleBackColor = true;
+            this.btn_update.Click += new System.EventHandler(this.btn_update_Click);
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.Location = new System.Drawing.Point(581, 176);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(75, 23);
+            this.btn_delete.TabIndex = 11;
+            this.btn_delete.Text = "Delete";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
+            // 
+            // btn_close
+            // 
+            this.btn_close.Location = new System.Drawing.Point(581, 230);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(75, 23);
+            this.btn_close.TabIndex = 12;
+            this.btn_close.Text = "Close";
+            this.btn_close.UseVisualStyleBackColor = true;
+            this.btn_close.Click += new System.EventHandler(this.btn_close_Click);
+            // 
+            // show
+            // 
+            this.show.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.show.Location = new System.Drawing.Point(261, 288);
+            this.show.Name = "show";
+            this.show.Size = new System.Drawing.Size(395, 150);
+            this.show.TabIndex = 13;
+            this.show.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.show_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 509);
+            this.Controls.Add(this.show);
+            this.Controls.Add(this.btn_close);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_update);
+            this.Controls.Add(this.btn_Add);
+            this.Controls.Add(this.txt_quantity);
+            this.Controls.Add(this.txt_price);
+            this.Controls.Add(this.txt_name);
+            this.Controls.Add(this.txt_id);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.show)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_id;
+        private System.Windows.Forms.TextBox txt_name;
+        private System.Windows.Forms.TextBox txt_price;
+        private System.Windows.Forms.TextBox txt_quantity;
+        private System.Windows.Forms.Button btn_Add;
+        private System.Windows.Forms.Button btn_update;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.Button btn_close;
+        private System.Windows.Forms.DataGridView show;
+    }
+}
+
---
+++ Call_API/Call_API/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Call_API
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void HienThi()
+        {
+            string link = "https://localhost:44383/api/Product";
+            HttpWebRequest request = HttpWebRequest.CreateHttp(link);
+            WebResponse response = request.GetResponse();
+            DataContractJsonSerializer js = new DataContractJsonSerializer(typeof(Products[]));
+            object data = js.ReadObject(response.GetResponseStream());
+            Products[] arrNV = data as Products[];
+            show.DataSource = arrNV;
+        }
+        private void btn_Add_Click(object sender, EventArgs e)
+        {
+            string postString = string.Format("?id={0}&name={1}&price={2}&quantity={3}", txt_id.Text, txt_name.Text, txt_price.Text,txt_quantity.Text);
+            string link = "https://localhost:44383/api/Product" + postString;
+            HttpWebRequest request = HttpWebRequest.CreateHttp(link);
+            request.Method = "POST";
+            request.ContentType = "application/json;charset=UTF-8";
+            byte[] arrByte = Encoding.UTF8.GetBytes(postString);
+            request.ContentLength = arrByte.Length;
+            Stream dataStream = request.GetRequestStream();
+            dataStream.Write(arrByte, 0, arrByte.Length);
+            dataStream.Close();
+            DataContractJsonSerializer js = new DataContractJsonSerializer(typeof(bool));
+            object data = js.ReadObject(request.GetResponse().GetResponseStream());
+            bool kq = (bool)data;
+            if (kq)
+            {
+                HienThi();
+                MessageBox.Show("Add success !");
+
+            }
+            else
+            {
+                MessageBox.Show("Add failed...");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            HienThi();
+        }
+
+        private void show_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            HienThi();
+            DataGridViewRow row = new DataGridViewRow();
+            row = show.Rows[e.RowIndex];
+            txt_id.Text = Convert.ToString(row.Cells[0].Value);
+            txt_name.Text = Convert.ToString(row.Cells[1].Value);
+            txt_price.Text = Convert.ToString(row.Cells[2].Value);
+            txt_quantity.Text = Convert.ToString(row.Cells[3].Value);
+        }
+
+        private void btn_update_Click(object sender, EventArgs e)
+        {
+
+            string postString = string.Format("?id={0}&name={1}&price={2}&quantity={3}", txt_id.Text, txt_name.Text, txt_price.Text, txt_quantity.Text);
+            string link = "https://localhost:44383/api/Product" + postString;
+            HttpWebRequest request = HttpWebRequest.CreateHttp(link);
+            request.Method = "PUT";
+            request.ContentType = "application/json;charset=UTF-8";
+            byte[] arrByte = Encoding.UTF8.GetBytes(postString);
+            request.ContentLength = arrByte.Length;
+            Stream dataStream = request.GetRequestStream();
+            dataStream.Write(arrByte, 0, arrByte.Length);
+            dataStream.Close();
+            DataContractJsonSerializer js = new DataContractJsonSerializer(typeof(bool));
+            object data = js.ReadObject(request.GetResponse().GetResponseStream());
+            bool kq = (bool)data;
+            if (kq)
+            {
+                HienThi();
+                MessageBox.Show("Update success !");
+
+            }
+            else
+            {
+                MessageBox.Show("Updtate failed...");
+            }
+        }
+
+        private void btn_delete_Click(object sender, EventArgs e)
+        {
+            string postString = string.Format("?id={0}", txt_id.Text);
+            string link = "https://localhost:44383/api/Product" + postString;
+            HttpWebRequest request = HttpWebRequest.CreateHttp(link);
+            request.Method = "DELETE";
+            request.ContentType = "application/json;charset=UTF-8";
+            byte[] arrByte = Encoding.UTF8.GetBytes(postString);
+            request.ContentLength = arrByte.Length;
+            Stream dataStream = request.GetRequestStream();
+            dataStream.Write(arrByte, 0, arrByte.Length);
+            dataStream.Close();
+            DataContractJsonSerializer js = new DataContractJsonSerializer(typeof(bool));
+            object data = js.ReadObject(request.GetResponse().GetResponseStream());
+            bool kq = (bool)data;
+            if (kq)
+            {
+                HienThi();
+                MessageBox.Show("Delete success !");
+
+            }
+            else
+            {
+                MessageBox.Show("Delete failed...");
+            }
+        }
+
+        private void btn_close_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
